a50107426bb87a94c70f1ece376e600fe69e3583
Robin KAY <komadori@gekkou.co.uk>
2023-01-14 02:45:56 +0000

    Change render_layers example to use point lighting since Bevy 0.8 bug is fixed.

--- examples/render_layers.rs
+++ examples/render_layers.rs
@@ -66,9 +66,14 @@ fn setup(
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
-    commands.insert_resource(AmbientLight {
-        color: Color::WHITE,
-        brightness: 1.0,
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
     });
 
     // Add cameras for different combinations of render-layers
