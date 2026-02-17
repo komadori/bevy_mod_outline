8b9c559a4c40b798413a5cbdf561d8e5e3fe0345
Robin KAY <komadori@gekkou.co.uk>
2026-02-17 12:56:46 +0000

    Fix clippy lint.

--- src/msaa.rs
+++ src/msaa.rs
@@ -66,7 +66,7 @@ impl ViewNode for MsaaExtraWritebackNode {
         let bind_group = blit_pipeline.create_bind_group(
             render_context.render_device(),
             post_process.source,
-            &pipeline_cache,
+            pipeline_cache,
         );
 
         let mut render_pass = render_context
